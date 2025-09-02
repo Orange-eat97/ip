52c1a76513520ad0ffd4599593bfda97f756d80d
Orange-eat97 <jameschi.0197@gmail.com>
2025-09-03 01:03:46 +0800

    Update CC.java

--- src/main/java/CC/CC.java
+++ src/main/java/CC/CC.java
@@ -8,18 +8,18 @@ public class CC {
     private Ui Ui;
     private Storage Storage;
 
-    public CC(String filePath) throws EmptyTimeException, WrongHeadingException {
+    public CC(String filePath) throws EmptyTimeException, WrongHeadingException {    //constructor for CC
         this.Storage = new Storage(filePath);
         this.Ui = new Ui();
     }
 
-    public void run() throws EmptyTimeException, WrongHeadingException {
+    public void run() throws EmptyTimeException, WrongHeadingException {        //gets CC bot in action
         Storage.ensureDataFileExists();
         Ui.Start();
         Ui.waitForCommand();
     }
 
-    public static void main(String[] args) throws WrongHeadingException, EmptyTimeException {
+    public static void main(String[] args) throws WrongHeadingException, EmptyTimeException {    //main method
         new CC("data/tasks.txt").run();
     }
 }
